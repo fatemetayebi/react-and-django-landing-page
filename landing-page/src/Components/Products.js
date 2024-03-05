26c48573f292b562311fad0eb10653881f4fe52c
fateme tayebi <52858721+sunken29@users.noreply.github.com>
2024-03-05 16:57:42 +0330

    140212151657

--- landing-page/src/Components/Products.js
+++ landing-page/src/Components/Products.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import WheelPicker from 'react-simple-wheel-picker';
@@ -15,7 +16,31 @@ const Product = () => {
         console.error(error);
       });
   }, []);
+=======
+import React from "react";
+import PickMeals from "../Assets/pick-meals-image.png";
+import ChooseMeals from "../Assets/choose-image.png";
+import DeliveryMeals from "../Assets/delivery-image.png";
+>>>>>>> parent of e4a964a (140212141138)
 
+const Work = () => {
+  const workInfoData = [
+    {
+      image: PickMeals,
+      title: "Pick Meals",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+    },
+    {
+      image: ChooseMeals,
+      title: "Choose How Often",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+    },
+    {
+      image: DeliveryMeals,
+      title: "Fast Deliveries",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+    },
+  ];
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
@@ -26,6 +51,7 @@ const Product = () => {
           elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
         </p>
       </div>
+<<<<<<< HEAD
       <div className='work-section-products'>
         <WheelPicker style={{ width: 200, height: 150 }}>
           {products.map((data) => (
@@ -38,9 +64,21 @@ const Product = () => {
             </div>
           ))}
         </WheelPicker>
+=======
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={data.image} alt="" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+>>>>>>> parent of e4a964a (140212141138)
       </div>
     </div>
   );
 };
 
-export default Product;
+export default Work;
